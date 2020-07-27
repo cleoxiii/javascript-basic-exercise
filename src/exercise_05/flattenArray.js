34e1d62c6c998d0f29fef1c58270a16fd7f21d79
Yun Chenxi <cleoxiii@163.com>
2020-07-27 21:53:50 +0800

    [yunchenxi] feat: flatten_array_spec - should flatten a nested array into a sequence.

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -5,6 +5,5 @@ export default function flattenArray(array) {
   //
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  return [].concat(...array);
 }
