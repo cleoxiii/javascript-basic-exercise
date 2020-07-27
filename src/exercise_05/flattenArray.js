04bbd22b58be7df05ecda5311223814137f0811f
Yun Chenxi <cleoxiii@163.com>
2020-07-27 21:55:43 +0800

    [yunchenxi] feat: flatten_array_spec - should throw if array is null or undefined.

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -5,5 +5,9 @@ export default function flattenArray(array) {
   //
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
+  if (array === undefined || array === null) {
+    throw new Error('Flatten undefined or null array');
+  }
+
   return [].concat(...array);
 }
